7183586f45aaf3e39858854d40f712fb3cc25837
kadkuk <77267320+kadkuk@users.noreply.github.com>
2021-01-13 15:59:37 +0200

    Commit Lesson1MathUtil

--- src/main/java/ee/bcs/valiit/tasks/Lesson1MathUtil.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson1MathUtil.java
@@ -1,5 +1,7 @@
 package ee.bcs.valiit.tasks;
 
+import org.ehcache.xml.model.ExpiryType;
+
 public class Lesson1MathUtil {
     private String test;
 
@@ -9,36 +11,70 @@ public class Lesson1MathUtil {
     }
 
     public static void main(String[] args) {
+        System.out.println(min(34, 5));
+        System.out.println(max(23, 66));
+        System.out.println(abs(-34));
+        System.out.println(min(4,4,5));
+        System.out.println(max(36,33,34));
+        System.out.println(isEven(43));
     }
 
     // TODO tagasta a ja b väikseim väärtus
     public static int min(int a, int b) {
-        return 0;
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
     }
 
     // TODO tagasta a ja b suurim väärtus
     public static int max(int a, int b) {
-        return 0;
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
     }
 
     // TODO tagasta a absoluut arv
     public static int abs(int a) {
-        return 0;
+        if (a < 0) {
+            return a + (-a * 2);
+        } else {
+            return a;
+        }
     }
 
     // TODO tagasta true, kui a on paaris arv
     // tagasta false kui a on paaritu arv
     public static boolean isEven(int a) {
-        return true;
+        if (a % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // TODO tagasta kolmest arvust kõige väiksem
     public static int min(int a, int b, int c) {
-        return 0;
+        if (a <= b && a <= c) {
+            return a;
+        } else if (b <= a && b <= c) {
+            return b;
+        } else {
+            return c;
+        }
     }
 
     // TODO tagasta kolmest arvust kõige suurem
     public static int max(int a, int b, int c) {
-        return 0;
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
+        } else {
+            return c;
+        }
     }
 }
