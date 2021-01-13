109800e0f7f300dc6ac8f91fbb06c63d8272f285
kadkuk <77267320+kadkuk@users.noreply.github.com>
2021-01-13 16:05:43 +0200

    Commit Lesson1 and Lesson3

--- src/main/java/ee/bcs/valiit/tasks/Lesson1.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson1.java
@@ -15,6 +15,16 @@ public class Lesson1 {
     // Lisa rida a = c
     // Prindi välja a==b ja a==c, mis muutus???
     public static void excersie1(){
+        int a=1;
+        int b=1;
+        int c= 3;
+        System.out.println( a == b );
+        System.out.println( a == c );
+        a=c;
+        System.out.println( a == b );
+        System.out.println( a == c );
+
+
     }
 
     // TODO
@@ -23,6 +33,14 @@ public class Lesson1 {
     // Tekita muutuja y2 = x2++, trüki välja nii x2 ja y2
     // Analüüsi tulemusi
     public static void excersie2(){
+        int x1 = 10;
+        int x2 = 20;
+        int y1 = ++x1;
+        int y2 = x2++;
+        System.out.println(x1);
+        System.out.println(y1);
+        System.out.println(x2);
+        System.out.println(y2);
     }
 
     // TODO
@@ -33,12 +51,20 @@ public class Lesson1 {
     // d = 21 % 3
     // Prindi välja kõigi muutujate väärtused
     public static void excersie3(){
+         int a = 18 % 3;
+         float b = 19 % 3;
+         float c = 20 % 3;
+         int d = 21 % 3;
+         System.out.println(a + " " + b + " " + c + " " + d);
     }
 
     // TODO
     // Defineeri String tüüpi muutuja mille sisu oleks "\"\\""
     // Trüki muutuja sisu välja
     public static void excersie4(){
+        String a = "\\";
+        String b = "\"\\\"\\\\\\\"\\\\\\\\\\\"\\\"\"";
+        System.out.println(b);
     }
 
 }
--- src/main/java/ee/bcs/valiit/tasks/Lesson3.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson3.java
@@ -2,35 +2,80 @@ package ee.bcs.valiit.tasks;
 
 public class Lesson3 {
     public static void main(String[] args) {
+        sum(new int[]{1, 2, 3, 4, 5, 6});
+        factorial(5);
+        sort(new int[]{5, 3, 7, 1, 100, 655, 9});
+        reverseString("Hallooo!");
+        isPrime(11);
 
     }
 
-    public static int sum(int[] x){
+    public static int sum(int[] x) {
         // Todo liida kokku kõik numbrid massivis x
-        return 0;
+        int summa = 0;
+        for (int i : x) {
+            summa = summa + i;
+        }
+        System.out.println("Summa on: " + summa);
+        return summa;
     }
 
     public static int factorial(int x) {
         // TODO tagasta x faktoriaal.
-        // Näiteks
-        // x = 5
-        // return 5*4*3*2*1 = 120
-        return 0;
+        int fac = 1;
+        int i = 2;
+        while (i <= x) {
+            fac *= i;
+            i++;
+        }
+        System.out.println(x + "-e faktoriaal on: " + fac);
+        return fac;
     }
 
     public static int[] sort(int[] a) {
         // TODO sorteeri massiiv suuruse järgi.
         // TODO kasuta tsükleid, ära kasuta ühtegi olemasolevat sort funktsiooni
-        return new int[0];
+        int[] srt = a;
+        for (int i = 0; i< srt.length; i++) {
+            for (int j=i+1; j< srt.length; j++) {
+                if (srt[i] > srt[j]) {
+                    int ajutine = srt[i];
+                    srt[i] = srt[j];
+                    srt[j] = ajutine;
+                }
+            }
+
+            System.out.print(srt[i] + " ");
+            }
+        return srt;
     }
 
     public static String reverseString(String a) {
         // TODO tagasta string tagurpidi
+        char[] sisendSrting = a.toCharArray();
+        int i = 0;
+        int j = a.length() - 1;
+        while (i < j) {
+            char ajutine = sisendSrting[i];
+            sisendSrting[i] = sisendSrting[j];
+            sisendSrting[j] = ajutine;
+            i++;
+            j--;
+        }
+        System.out.println("\n");
+        System.out.println(sisendSrting);
         return "";
     }
 
-    public static boolean isPrime(int x){
+    public static boolean isPrime(int x) {
         // TODO tagasta kas sisestatud arv on primaar arv (jagub ainult 1 ja iseendaga)
-        return false;
+        for (int i = 2; i < x; i++) {
+            if (x % i == 0) {
+                System.out.println(x + " ei ole algarv.");
+                return false;
+            }
+        }
+        System.out.println(x + " on algarv.");
+        return true;
     }
 }
