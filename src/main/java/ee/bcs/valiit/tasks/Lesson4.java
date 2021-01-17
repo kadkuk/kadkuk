19d9eccea4080d022addae71b4fbca61ddb45e44
kadkuk <77267320+kadkuk@users.noreply.github.com>
2021-01-17 17:17:18 +0200

    Lesson 4 with improved rounding rules.

--- src/main/java/ee/bcs/valiit/tasks/Lesson4.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson4.java
@@ -1,7 +1,7 @@
 package ee.bcs.valiit.tasks;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -66,16 +66,15 @@ public class Lesson4 {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please enter your account number: ");
             String accNumber = scanner.nextLine();
-            MathContext round = new MathContext(4);
             if (!accountBalanceMap.containsKey(accNumber)) {
                 System.out.println("Account not found.");
             } else {
                 System.out.println("Please enter the amount: ");
                 String summa = scanner.nextLine();
-                BigDecimal uus = new BigDecimal(summa);
-                BigDecimal balance = accountBalanceMap.get(accNumber);
+                BigDecimal uus = new BigDecimal(summa).setScale(2, RoundingMode.HALF_UP);
+                BigDecimal balance = accountBalanceMap.get(accNumber).setScale(2, RoundingMode.HALF_UP);
                 if (uus.compareTo(BigDecimal.ZERO) > 0) {
-                    accountBalanceMap.put(accNumber, (uus.add(balance, round)));
+                    accountBalanceMap.put(accNumber, (uus.add(balance)));
                     System.out.println("Your new balance is: " + (uus.add(balance)) + " EUR");
                 } else {
                     System.out.println("Non-suitable entry");
@@ -87,16 +86,15 @@ public class Lesson4 {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please enter your account number: ");
             String accNumber = scanner.nextLine();
-            MathContext round = new MathContext(4);
             if (!accountBalanceMap.containsKey(accNumber)) {
                 System.out.println("Account not found.");
             } else {
                 System.out.println("Please enter the amount: ");
                 String summa = scanner.nextLine();
-                BigDecimal uus = new BigDecimal(summa);
-                BigDecimal balance = accountBalanceMap.get(accNumber);
+                BigDecimal uus = new BigDecimal(summa).setScale(2, RoundingMode.HALF_UP);
+                BigDecimal balance = accountBalanceMap.get(accNumber).setScale(2, RoundingMode.HALF_UP);
                 if ((uus.compareTo(BigDecimal.ZERO)) > 0 && (balance.compareTo(uus)) >= 0) {
-                    accountBalanceMap.put(accNumber, (balance.subtract(uus, round)));
+                    accountBalanceMap.put(accNumber, (balance.subtract(uus)));
                     System.out.println("Your new balance is: " + accountBalanceMap.get(accNumber) + " EUR");
                 } else {
                     System.out.println("Transaction not allowed.");
@@ -109,20 +107,19 @@ public class Lesson4 {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please enter your account number: ");
             String accNumber = scanner.nextLine();
-            MathContext round = new MathContext(4);
             if (!accountBalanceMap.containsKey(accNumber)) {
                 System.out.println("Account not found.");
             } else if (accountBalanceMap.containsKey(accNumber)) {
                 System.out.println("Please enter destination account number: ");
                 String toAccount = scanner.nextLine();
                 accountBalanceMap.put(toAccount, BigDecimal.ZERO);
-                BigDecimal balance = accountBalanceMap.get(accNumber);
+                BigDecimal balance = accountBalanceMap.get(accNumber).setScale(2, RoundingMode.HALF_UP);
                 System.out.println("Please enter the amount: ");
                 String summa = scanner.nextLine();
-                BigDecimal uus = new BigDecimal(summa);
+                BigDecimal uus = new BigDecimal(summa).setScale(2, RoundingMode.HALF_UP);
                 if ((uus.compareTo(BigDecimal.ZERO)) > 0 && (balance.compareTo(uus)) >= 0) {
-                    accountBalanceMap.put(toAccount, (BigDecimal.ZERO.add(uus, round)));
-                    accountBalanceMap.put(accNumber, (balance.subtract(uus, round)));
+                    accountBalanceMap.put(toAccount, (BigDecimal.ZERO.add(uus)));
+                    accountBalanceMap.put(accNumber, (balance.subtract(uus)));
                     System.out.println("Your new balance is: " + accountBalanceMap.get(accNumber) + " EUR");
                     System.out.println("Destination account new balance is: " + accountBalanceMap.get(toAccount) + " EUR");
                 } else {
